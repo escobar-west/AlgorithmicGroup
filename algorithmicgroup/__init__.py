2fff4f5598781581ac3b5e61ea93666619479ce4
Victor Suriel <escobar@Victors-MacBook-Pro-2.local>
2017-09-16 22:15:52 -0400

    changed module name from indicators to assets

--- algorithmicgroup/__init__.py
+++ algorithmicgroup/__init__.py
@@ -1,3 +1,3 @@
-from algorithmicgroup.indicators import Asset, Stock, BTC
+from algorithmicgroup.assets import Asset, Stock, BTC
 import algorithmicgroup.signals as sig
 from algorithmicgroup.backtest import backtest_strat
